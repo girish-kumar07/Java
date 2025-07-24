f54bf43145b6df5e899babec39ae3440c7893aae
Girish Kumar <girish.kumar3517@gmail.com>
2025-07-24 09:52:23 -0700

    Program to check the entered number is Palindrome or Not.

---
+++ palindromeNumber.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class palindromeNumber {
+
+    public static void isPalindrome(int number){
+        int myNum = number;
+        int palinNum = 0; // This variable will store the Palindrome number.
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            palinNum = (palinNum * 10) + lastDigit;
+            number /= 10;
+        }
+        if (myNum == palinNum){
+            System.out.println("Entered number " + myNum + " is  Palindrome");
+        }
+        else{
+            System.out.println("Entered number " + myNum + " is  NOT  Palindrome");
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter your number: ");
+        int n = sc.nextInt();
+        isPalindrome(n);
+    }
+}
