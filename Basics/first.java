7051b6254acf71a20fd75215535e28c91fa9c862
girish-kumar07 <girish.kumar3517@gmail.com>
2025-10-20 00:17:47 +0530

    Add Basics folder with Java files

---
+++ Basics/first.java
@@ -0,0 +1,7 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.print("My First Java Program");
+        System.out.print("\nUsing new line function\n");
+        System.out.println("Using println function");
+    }
+}
